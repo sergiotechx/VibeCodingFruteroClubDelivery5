4e63b4de142cf30d068e962dad43947f1a38fbee
Sergio Martinez Marin <sergiotechx@yahoo.com>
2026-02-19 23:35:32 -0500

    Ahora si

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PrivyProvider } from '@privy-io/react-auth'
-import 'nes.css/css/nes.min.css'
+import './retro.css'
 import './index.css'
 import App from './App.tsx'
 
